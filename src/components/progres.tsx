248d8469950c8459517685f84a3bcc723ff52e44
didadida262 <719713496@qq.com>
2024-09-12 18:19:41 +0800

    some_modify

--- src/components/progres.tsx
+++ src/components/progres.tsx
@@ -4,15 +4,29 @@
  * @Author: didadida262
  * @Date: 2024-09-12 16:29:20
  * @LastEditors: didadida262
- * @LastEditTime: 2024-09-12 17:08:10
+ * @LastEditTime: 2024-09-12 18:19:30
  */
 import cn from "classnames";
 import { useEffect, useState } from "react";
 
 export function Progress() {
   const [progress, setProgress] = useState(0);
+  const [suffix, setSuffix] = useState("");
   const total = 520;
   useEffect(() => {
+    const suffixTimer = setInterval(() => {
+      setSuffix(prevText => {
+        if (prevText === "") {
+          return ".";
+        } else if (prevText === ".") {
+          return "..";
+        } else if (prevText === "..") {
+          return "...";
+        } else {
+          return "";
+        }
+      });
+    }, 500);
     const timer = setInterval(() => {
       setProgress(prevProgress => {
         if (prevProgress < total) {
@@ -28,13 +42,18 @@ export function Progress() {
         }
       });
     }, 1000);
-    return () => clearInterval(timer);
+    return () => {
+      clearInterval(timer);
+      clearInterval(suffixTimer);
+    };
   }, []);
   return (
     <div className={cn(` flex h-full w-full items-center justify-center `)}>
       <div className=" w-[600px] flex flex-col items-center">
         <div className="h-[22px] w-full flex justify-center items-center ">
-          <span className="text-[18px] text-[#FFFFFF]">内容加载中...</span>
+          <span className="text-[18px] text-[#FFFFFF]">
+            {"内容加载中" + suffix}
+          </span>
         </div>
 
         <div className="container h-[10px] w-full flex justify-between items-center mt-[10px]">
