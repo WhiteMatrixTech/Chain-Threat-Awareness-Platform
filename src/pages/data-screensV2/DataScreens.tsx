ab8ab6115adb143454075631390f0691484e397d
didadida262 <719713496@qq.com>
2024-09-14 10:06:36 +0800

    大屏

--- src/pages/data-screensV2/DataScreens.tsx
+++ src/pages/data-screensV2/DataScreens.tsx
@@ -5,7 +5,7 @@
  * @Author: didadida262
  * @Date: 2024-09-03 17:58:07
  * @LastEditors: didadida262
- * @LastEditTime: 2024-09-14 09:57:00
+ * @LastEditTime: 2024-09-14 10:06:21
  */
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
@@ -295,7 +295,7 @@ export function DataScreens(props: dataScreensProps) {
           </div>
         </div>
         <div className=" w-[calc(100%_-_903px)] h-full flex justify-between flex-col items-center">
-          <div className="earthContainer w-full h-[calc(100%_-_196px)]" />
+          <div className="earthContainer w-full h-[calc(100%_-_196px)]  flex justify-center items-center" />
           <div className="w-full min-w-[850px] h-[196px] flex justify-between items-center 3xl:scale-100 scale-50 transform origin-bottom">
             {middleBottomList.map((item: any, index: number) =>
               <div
