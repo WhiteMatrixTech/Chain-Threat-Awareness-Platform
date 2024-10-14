4e88c5f41afd2b549cee764baed91f1cd28a1f0e
didadida262 <719713496@qq.com>
2024-10-14 16:12:51 +0800

    add:全模块导航tips新增

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -1,17 +1,21 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable react-hooks/rules-of-hooks */
 /* eslint-disable prettier/prettier */
 /*
  * @Description:
  * @Author: didadida262
  * @Date: 2024-08-26 10:34:32
  * @LastEditors: didadida262
- * @LastEditTime: 2024-09-18 12:13:13
+ * @LastEditTime: 2024-10-14 16:08:56
  */
 import { Image, Layout } from "antd";
 import cn from "classnames";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router";
 
 import { Header } from "@/components/Header";
+import { MenuList } from "@/components/LeftMenu/_menu";
 import { UserProvider } from "@/services/context";
 
 import styles from "./AppLayout.module.less";
@@ -20,6 +24,7 @@ const { Content, Sider } = Layout;
 
 export function AppLayout({ children }: { children: React.ReactNode }) {
   const { pathname } = useLocation();
+  const [navInfo, setnavInfo] = useState({ start: "", end: "" });
   const withoutLayout = ["/login", "/login/", "/register/", "/register"];
 
   if (withoutLayout.includes(pathname)) {
@@ -34,29 +39,47 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
     );
   }
 
+  useEffect(
+    () => {
+      console.log("pathname>>", pathname);
+      let start = "";
+      let end = "";
+      if (pathname.includes("/threat-detection")) {
+        start = "威胁感知";
+        const targetNav = MenuList.filter(
+          (item: any) => item.key === "/threat-detection"
+        )[0];
+        end = targetNav.children.filter((item: any) =>
+          pathname.includes(item.key)
+        )[0].label;
+      } else if (pathname.includes("/threat-evidence")) {
+        start = "威胁取证";
+        const targetNav = MenuList.filter(
+          (item: any) => item.key === "/threat-evidence"
+        )[0];
+        end = targetNav.children.filter((item: any) =>
+          pathname.includes(item.key)
+        )[0].label;
+      }
+      setnavInfo({
+        start: start,
+        end: end
+      });
+    },
+    [pathname]
+  );
+
   return (
     <UserProvider>
       <div className=" h-screen overflow-hidden relative pt-[23px] pb-[30px] bg-[url('./assets/bgNew_webp.webp')] bg-cover bg-center">
-        {/* <Sider
-          theme="light"
-          breakpoint="lg"
-          collapsedWidth="0"
-          className={cn('w-[265px] max-w-[265px]', styles.layoutAside)}
-          collapsible={true}
-          collapsed={collapsed}
-          onCollapse={(value) => setCollapsed(value)}
-        >
-          <a
-            className="text-white flex w-full items-center justify-center py-3"
-            href="/"
-          >
-            <span className="sr-only">Home</span>
-            <Image width={180} src={logo} preview={false} />
-          </a>
-          <LeftMenu />
-        </Sider> */}
         <Header />
-        <div className="mt-[36px] overflow-y-auto overflow-x-hidden px-[40px] h-[calc(100%_-_100px)]">
+
+        <div className="pt-[36px] overflow-y-auto overflow-x-hidden px-[40px] h-[calc(100%_-_64px)] relative">
+          {navInfo.start.length !== 0 &&
+            <div className="absolute top-0 left-0 text-[#EFF4FF] text-[20px] px-[31px]">
+              {navInfo.start + "-" + navInfo.end}
+            </div>}
+
           {children}
         </div>
       </div>
