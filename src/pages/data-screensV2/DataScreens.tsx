7617545e7922efea54270877c2d345b441e8fd4d
didadida262 <719713496@qq.com>
2024-10-16 17:56:11 +0800

    add:大屏时分秒

--- src/pages/data-screensV2/DataScreens.tsx
+++ src/pages/data-screensV2/DataScreens.tsx
@@ -153,7 +153,10 @@ export function DataScreens(props: dataScreensProps) {
     const year = currentDate.getFullYear();
     const month = String(currentDate.getMonth() + 1).padStart(2, "0"); // 月份从0开始，需要加1并补零
     const day = String(currentDate.getDate()).padStart(2, "0"); // 补零
-    const formattedDate = `${year}年${month}月${day}日`;
+    const hours = String(currentDate.getHours()).padStart(2, "0");
+    const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+    const seconds = String(currentDate.getSeconds()).padStart(2, "0");
+    const formattedDate = `${year}年${month}月${day}日${hours}:${minutes}:${seconds}`;
     setDate(formattedDate);
   };
   const writeData = (data: any) => {
@@ -207,9 +210,13 @@ export function DataScreens(props: dataScreensProps) {
     )[0];
     setChartLineAddressData(detect_address.lineDataList);
   };
+  const listenTime = () => {
+    getDate();
+    requestAnimationFrame(listenTime);
+  };
 
   useEffect(() => {
-    getDate();
+    listenTime();
     const timer = setInterval(async () => {
       const response = await dataScreenService();
       console.log("repose>>>", response);
