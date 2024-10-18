4416432e889b7794cd8e7b74a536255a13c142b3
didadida262 <719713496@qq.com>
2024-10-18 10:20:44 +0800

    add:柱状图颜色修改

--- src/pages/detection-chart-afterChain/DetectionStatisticColumn.tsx
+++ src/pages/detection-chart-afterChain/DetectionStatisticColumn.tsx
@@ -49,7 +49,19 @@ const data = [
     amount: 3
   }
 ];
-
+const colors: any = [
+  "#34A853", // 舒适的绿色
+  "#6298F4", // 柔和的蓝色灰
+  "#F9D95E", // 鲜艳的红色
+  "#9B59B6", // 柔和的紫色
+  "#2ECC71", // 鲜艳的绿色
+  "#F1C40F", // 明亮的黄色
+  "#E67E22", // 鲜艳的橙色
+  "#3498DB", // 鲜艳的蓝色
+  "#95A5A6", // 浅灰色调
+  "#FE7979", // 清爽的绿色
+  "#D35400" // 橙红色
+];
 const DemoColumn = () => {
   const [searchParams] = useSearchParams();
   let DetectionResults: any[] = [];
@@ -106,6 +118,11 @@ const DemoColumn = () => {
         amount: DetectionResults.filter(o => o.swcId === 124).length
       }
     ],
+    columnStyle: (item: any) => {
+      // return { fill: item.color }; // 其他类型的柱子颜色为蓝色
+      console.log(item);
+      return { fill: colors[Number(item.projectNumber) - 1] }; // 其他类型的柱子颜色为蓝色
+    },
     height: 200,
     xField: "projectNumber",
     yField: "amount",
